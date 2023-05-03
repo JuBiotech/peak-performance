8c403bc18a5681ac816c6d3686af8afb37895f1c
j.niesser <j.niesser@fz-juelich.de>
2023-05-03 09:15:43 +0200

    added model skew normal with working height; area and height tested

--- pipeline.py
+++ pipeline.py
@@ -48,14 +48,14 @@ def prefiltering(t_ret, sn_min):
     pass
 
 
-def initial_guesses(time_np, intensity, unique_identifier):
+def initial_guesses(time_np, intensity_np, unique_identifier):
     """
     Provide initial guesses for priors
     """
     # select lowest third of all data points as noise -> noise_tuple
-    intensity_tuple = list(enumerate(intensity))
+    intensity_tuple = list(enumerate(intensity_np))
     intensity_tuple.sort(key=lambda x: x[1])
-    noise_range = int(np.round(1 / 3 * len(intensity), decimals=0))
+    noise_range = int(np.round(1 / 3 * len(intensity_np), decimals=0))
     noise_tuple = intensity_tuple[:noise_range]
     # sort noise_tuple by time, then use the first and last data points for estimating starting values for the priors of noise width and the slope and intercept of baseline
     noise_tuple.sort(key=lambda x: x[0])
@@ -75,12 +75,12 @@ def initial_guesses(time_np, intensity, unique_identifier):
     return intercept_guess, slope_guess, noise_width_guess
 
 
-def define_model_normal(time_np, intensity, unique_identifier):
+def define_model_normal(time_np, intensity_np, unique_identifier):
     """
     Define a model for fitting a normal distribution to the peak data.
     """
     intercept_guess, slope_guess, noise_width_guess = initial_guesses(
-        time_np, intensity, unique_identifier
+        time_np, intensity_np, unique_identifier
     )
     with pm.Model() as pmodel:
         # priors plus error handling in case of mathematically impermissible values
@@ -99,7 +99,7 @@ def define_model_normal(time_np, intensity, unique_identifier):
             noise = pm.LogNormal("noise", np.log(10), 1)
         mean = pm.Normal("mean", np.mean(time_np[[0, -1]]), np.ptp(time_np) / 2)
         std = pm.HalfNormal("std", np.ptp(time_np) / 3)
-        height = pm.HalfNormal("height", 0.95 * np.max(intensity))
+        height = pm.HalfNormal("height", 0.95 * np.max(intensity_np))
         area = pm.Deterministic("area", height / (1 / (std * np.sqrt(2 * np.pi))))
         sn = pm.Deterministic("sn", height / noise)
         # posterior
@@ -107,17 +107,17 @@ def define_model_normal(time_np, intensity, unique_identifier):
         y = pm.Deterministic("y", y)
 
         # likelihood (auf der y-Achse liegende Normalverteilung um Datenpunkte, entspricht hier Normalverteilung um Wert mit Standardabweichung = noise)
-        L = pm.Normal("L", mu=y, sigma=noise, observed=intensity)
+        L = pm.Normal("L", mu=y, sigma=noise, observed=intensity_np)
 
     return pmodel
 
 
-def define_model_doublepeak(time_np, intensity):
+def define_model_doublepeak(time_np, intensity_np):
     """
     Define a model for fitting two ordered normal distributions to the peak data (for when data contains two peaks or a double peak without baseline separation).
     """
     intercept_guess, slope_guess, noise_width_guess = initial_guesses(
-        time_np, intensity, unique_identifier
+        time_np, intensity_np, unique_identifier
     )
     with pm.Model() as pmodel:
         # priors (two peaks, hence two sets of parameters)
@@ -125,8 +125,8 @@ def define_model_doublepeak(time_np, intensity):
         noise = pm.LogNormal("noise", np.log(100), 1)
         std = pm.HalfNormal("std", np.ptp(time_np) / 3)
         std2 = pm.HalfNormal("std2", np.ptp(time_np) / 3)
-        height = pm.HalfNormal("height", 0.95 * np.max(intensity))
-        height2 = pm.HalfNormal("height2", 0.95 * np.max(intensity))
+        height = pm.HalfNormal("height", 0.95 * np.max(intensity_np))
+        height2 = pm.HalfNormal("height2", 0.95 * np.max(intensity_np))
         area = pm.Deterministic("area", height / (1 / (std * np.sqrt(2 * np.pi))))
         area2 = pm.Deterministic("area2", height2 / (1 / (std2 * np.sqrt(2 * np.pi))))
         # use univariate ordered normal distribution
@@ -146,15 +146,68 @@ def define_model_doublepeak(time_np, intensity):
         y = pm.Deterministic("y", y)
 
         # likelihood
-        L = pm.Normal("L", mu=y, sigma=noise, observed=intensity)
+        L = pm.Normal("L", mu=y, sigma=noise, observed=intensity_np)
 
     return pmodel
 
 
-def define_model_skew():
+def define_model_skew(time_np, intensity_np):
     """
     Define a model for fitting a skew normal distribution to the peak data.
     """
+    with pm.Model() as pmodel:
+        # priors
+        baseline = pm.Normal("baseline", 200, 100)
+        noise = pm.LogNormal("noise", np.log(100), 1)
+        mean = pm.Normal("mean", np.mean(time_np[[0, -1]]), np.ptp(time_np) / 2)
+        std = pm.HalfNormal("std", np.ptp(time_np) / 3)
+        alpha = pm.HalfNormal("alpha", 2.5)
+        area = pm.HalfNormal("area", np.max(intensity_np) * 0.9)
+        # calculate standard deviation of skew normal
+        std_skew = pm.Deterministic(
+            "std_skew", np.sqrt(std**2 * (1 - (2 * alpha**2) / ((alpha**2 + 1) * np.pi)))
+        )
+
+        # height is defined as the posterior with x = mode
+        # (difference to normal distribution: for normal distribution mean and mode are identical and inserting x = mean = mode leads to a simplification of the PDF)
+        # first calculate the mode (via calculating the mean of a skew normal and using a numerical approach to calculating the offset between mean and mode)
+        mean_skew = pm.Deterministic(
+            "mean_skew", mean + std * np.sqrt(2 / np.pi) * alpha / (np.sqrt(1 + alpha**2))
+        )
+        mue_z = pm.Deterministic("mue_z", np.sqrt(2 / np.pi) * alpha / (np.sqrt(1 + alpha**2)))
+        sigma_z = pm.Deterministic("sigma_z", np.sqrt(1 - mue_z**2))
+        fit_skewness = st.skew(intensity_np)
+        # this formula originally contained the sign() function which led to an error -> use alpha/abs(alpha) instead for the same effect
+        mode_offset = pm.Deterministic(
+            "mode_offset",
+            mue_z
+            - (fit_skewness * sigma_z) / 2
+            - (alpha / abs(alpha)) / 2 * pt.exp(-(2 * np.pi) / abs(alpha)),
+        )
+        # if alpha < 0: mode = mean + offset; if alpha > 0: mode = mean - offset;
+        mode_skew = pm.Deterministic("mode_skew", mean_skew - (alpha / abs(alpha)) * mode_offset)
+        # then calculate the height based on the mode
+        pm.Deterministic(
+            "height",
+            area
+            * (
+                2
+                * (1 / (std * np.sqrt(2 * np.pi)) * pt.exp(-0.5 * ((mode_skew - mean) / std) ** 2))
+                * (0.5 * (1 + pt.erf(((alpha * (mode_skew - mean) / std)) / np.sqrt(2))))
+            ),
+        )
+
+        # posterior
+        y = baseline + area * (
+            2
+            * (1 / (std * np.sqrt(2 * np.pi)) * pt.exp(-0.5 * ((time_np - mean) / std) ** 2))
+            * (0.5 * (1 + pt.erf(((alpha * (time_np - mean) / std)) / np.sqrt(2))))
+        )
+
+        y = pm.Deterministic("y", y)
+
+        # likelihood
+        L = pm.Normal("L", mu=y, sigma=noise, observed=intensity_np)
     pass
 
 
