8ed9face8a583286c71ce65252f17f21ca48611e
j.niesser <j.niesser@fz-juelich.de>
2023-05-31 15:50:18 +0200

    rearrange structure and add test for initial_guesses()

--- peak_performance/__init__.py
+++ peak_performance/__init__.py
@@ -1,4 +1,6 @@
 import importlib.metadata
+from . import models
+from . import plots
 
 __version__ = importlib.metadata.version(__package__ or __name__)
 """Package version when the install command ran."""
