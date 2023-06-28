345278b13044c516459e442edb5319fe253dae07
j.niesser <j.niesser@fz-juelich.de>
2023-06-28 16:19:28 +0200

    add pm.model() as info to model definition functions

--- peak_performance/models.py
+++ peak_performance/models.py
@@ -85,7 +85,7 @@ def normal_posterior(baseline, height, time: np.ndarray, mean, std):
     return baseline + height * pt.exp(-0.5 * ((time - mean) / std) ** 2)
 
 
-def define_model_normal(ui: pi.UserInput):
+def define_model_normal(ui: pi.UserInput) -> pm.Model:
     """
     Define a model for fitting a normal distribution to the peak data.
 
@@ -167,7 +167,7 @@ def double_normal_posterior(baseline, height, height2, time: np.ndarray, mean, s
     return y
 
 
-def define_model_doublepeak(ui: pi.UserInput):
+def define_model_doublepeak(ui: pi.UserInput) -> pm.Model:
     """
     Define a model for fitting two ordered normal distributions to the peak data (for when data contains two peaks or a double peak without baseline separation).
 
@@ -312,7 +312,7 @@ def skew_normal_posterior(baseline, area, time, mean, std, alpha):
     return y
 
 
-def define_model_skew(ui: pi.UserInput):
+def define_model_skew(ui: pi.UserInput) -> pm.Model:
     """
     Define a model for fitting a skew normal distribution to the peak data.
 
