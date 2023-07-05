5e6190a1ec5d48621e8e52c04ea94d2e8b15a1f3
j.niesser <j.niesser@fz-juelich.de>
2023-07-05 09:57:01 +0200

    create structure for unit tests of pipeline.py

---
+++ peak_performance/test_pipeline.py
@@ -0,0 +1,37 @@
+import numpy as np
+import pytest
+
+from peak_performance import pipeline
+
+
+def test_user_input_class():
+    pass
+
+
+def test_detect_raw_data():
+    pass
+
+
+def test_initiate():
+    pass
+
+
+def test_prefiltering():
+    pass
+
+
+def test_postfiltering():
+    pass
+
+
+def test_report_add_nan_to_summary():
+    pass
+
+
+def test_report_add_data_to_summary():
+    pass
+
+
+def test_pipeline_loop():
+    # make sure the correctly named files are there; test the report sheets for similarity
+    pass
